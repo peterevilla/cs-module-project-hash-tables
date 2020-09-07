02d67c943ddc853a845549496176e996b1b49ced
Pedro Revilla <peterevilla@gmail.com>
2020-09-06 19:50:52 -0500

    wip

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -97,7 +97,7 @@ class HashTable:
             while current is not None:
                 if current.key == key:
                     current.value = value
-                if current.key is None:
+                if current.next is None:
                     current.next = HashTableEntry(key, value)
                     return 
                 current = current.next
@@ -125,7 +125,13 @@ class HashTable:
         """
         # Your code here
         index = self.hash_index(key)
-        return self.bucket[index]
+        current = self.bucket[index]
+        if current.next is None:
+            return current.value
+        else:
+            while current is not None:
+                if current.key == key:
+                    return current.value
 
 
     def resize(self, new_capacity):
@@ -139,37 +145,50 @@ class HashTable:
 
 
 
-if __name__ == "__main__":
-    ht = HashTable(8)
+# if __name__ == "__main__":
+#     ht = HashTable(8)
 
-    ht.put("line_1", "'Twas brillig, and the slithy toves")
-    ht.put("line_2", "Did gyre and gimble in the wabe:")
-    ht.put("line_3", "All mimsy were the borogoves,")
-    ht.put("line_4", "And the mome raths outgrabe.")
-    ht.put("line_5", '"Beware the Jabberwock, my son!')
-    ht.put("line_6", "The jaws that bite, the claws that catch!")
-    ht.put("line_7", "Beware the Jubjub bird, and shun")
-    ht.put("line_8", 'The frumious Bandersnatch!"')
-    ht.put("line_9", "He took his vorpal sword in hand;")
-    ht.put("line_10", "Long time the manxome foe he sought--")
-    ht.put("line_11", "So rested he by the Tumtum tree")
-    ht.put("line_12", "And stood awhile in thought.")
+#     ht.put("line_1", "'Twas brillig, and the slithy toves")
+#     ht.put("line_2", "Did gyre and gimble in the wabe:")
+#     ht.put("line_3", "All mimsy were the borogoves,")
+#     ht.put("line_4", "And the mome raths outgrabe.")
+#     ht.put("line_5", '"Beware the Jabberwock, my son!')
+#     ht.put("line_6", "The jaws that bite, the claws that catch!")
+#     ht.put("line_7", "Beware the Jubjub bird, and shun")
+#     ht.put("line_8", 'The frumious Bandersnatch!"')
+#     ht.put("line_9", "He took his vorpal sword in hand;")
+#     ht.put("line_10", "Long time the manxome foe he sought--")
+#     ht.put("line_11", "So rested he by the Tumtum tree")
+#     ht.put("line_12", "And stood awhile in thought.")
 
-    print("")
+#     print("")
 
-    # Test storing beyond capacity
-    for i in range(1, 13):
-        print(ht.get(f"line_{i}"))
+#     # Test storing beyond capacity
+#     for i in range(1, 13):
+#         print(ht.get(f"line_{i}"))
 
-    # Test resizing
-    old_capacity = ht.get_num_slots()
-    ht.resize(ht.capacity * 2)
-    new_capacity = ht.get_num_slots()
+#     # Test resizing
+#     old_capacity = ht.get_num_slots()
+#     ht.resize(ht.capacity * 2)
+#     new_capacity = ht.get_num_slots()
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+#     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    # Test if data intact after resizing
-    for i in range(1, 13):
-        print(ht.get(f"line_{i}"))
+#     # Test if data intact after resizing
+#     for i in range(1, 13):
+#         print(ht.get(f"line_{i}"))
 
-    print("")
+#     print("")
+
+
+
+ht = HashTable(8)
+
+ht.put("line_1", "'Twas brillig, and the slithy toves")
+ht.put("line_2", "Did gyre and gimble in the wabe:")
+ht.put("line_3", "All mimsy were the borogoves,")
+
+
+for i in range(1, 4):
+    print(ht.get(f"line_{i}"))
+
